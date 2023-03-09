41a58ea1c62ead80bf641c82f8e53295aa3568bf
Sergey Dolin <dsame@github.com>
2023-03-09 06:48:17 +0100

    deps

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Uuids;
+
+var uuid = Uuid.NewTimeBased();
+Console.WriteLine(uuid.ToString());
